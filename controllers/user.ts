eb2cd68f2d50ad18018c00816ea44fc10f56b6f7
U-DESKTOP-58PJ9OE\tjcol <tjcold@hotmail.com>
2020-03-22 18:58:56 +0100

    Add routes for register and login

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,16 +1,53 @@
 import express from 'express';
-import  {User} from '../models/User';
+import { User } from '../models/User';
+import Bcrypt from 'bcrypt';
+
 const router = express.Router();
-router.post('/user', (req, res) => {
-  const {username, password } = req.body;
-  User.create({
-    username,
-    password
-  })
-    .then(user => {
-      res.json(user);
-    });
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({where: {
+    username
+  }});
+  if(user){
+    res.send('User already exists');
+  }
+  else {
+    const hashedPassword = await Bcrypt.hash(password, 10);
+    User.create({
+      username,
+      password: hashedPassword,
+      createdAt: Date.now()
+    })
+      .then(user => {
+        res.json(user);
+      });
+  }
 });
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      username
+    }
+  });
+  if(!user){
+    res.send('Invalid username or password');
+  }
+  else{
+    Bcrypt.compare(password, user.password)
+      .then(result => {
+        if(result){
+          res.json(user);
+        }
+        else{
+          res.send('Incorrect username or password');
+        }
+      });
+  }
+});
+
 router.get('/users', (req, res) => {
   User.findAll()
     .then(data => {
