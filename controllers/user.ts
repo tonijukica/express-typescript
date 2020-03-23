bf4fce1b8314748694d3f08afdacb473c83141c0
U-DESKTOP-58PJ9OE\tjcol <tjcold@hotmail.com>
2020-03-23 18:05:00 +0100

    add jwt check

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { User } from '../models/User';
 import Bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -39,7 +40,10 @@ router.post('/login', async (req, res) => {
     Bcrypt.compare(password, user.password)
       .then(result => {
         if(result){
-          res.json(user);
+          const token = jwt.sign({name: user.name}, process.env.TOKEN_SECRET);
+          res.json({
+            token
+          });
         }
         else{
           res.send('Incorrect username or password');
@@ -48,11 +52,29 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.get('/users', (req, res) => {
+const authJTW = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if(authHeader) {
+    const token = authHeader.split(' ')[1];
+    console.log(token);
+    const tokenSecret = process.env.TOKEN_SECRET;
+    jwt.verify(token, tokenSecret, (err, user) => {
+      if(err)
+        return res.send('Invalid token');
+      req.user = user;
+      next();
+    });
+  }
+  else
+    res.send('No authorization headers');
+};
+
+router.get('/users', authJTW, (req, res) => {
   User.findAll()
     .then(data => {
       res.json(data);
     });
 });
+
 
 export default router;
